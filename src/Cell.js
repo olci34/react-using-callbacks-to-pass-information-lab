e10f4180ff11eb5942be932ab4b99da3e1cdce0a
github username <github email address>
2021-05-13 20:07:01 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -4,14 +4,15 @@ export default class Cell extends Component {
   
   constructor(props) {
     super(props)
-    this.state = {
-      color: this.props.color
-    }
+    this.state = {color: this.props.color}
+    this.handleClick = this.handleClick.bind(this)
   }
-  
+  handleClick() {
+    this.setState({color: this.props.selectedColor})
+  }
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div onClick={this.handleClick} className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
--- src/Matrix.js
+++ src/Matrix.js
@@ -7,10 +7,16 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {cellColor: '#FFF'}
+    this.setCellColor = this.setCellColor.bind(this)
+  }
+
+  setCellColor(hex) {
+    this.setState({cellColor: hex})
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.cellColor}/>)
   )
 
   genMatrix = () => (
@@ -20,7 +26,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector selectedColor={this.setCellColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
